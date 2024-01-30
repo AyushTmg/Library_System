601f464a3525154b00d4d593231414fdbd49109c
AyushTmg <ayushtmg20040722@gmail.com>
2024-01-30 15:07:47 +0545

    Feat: Admin interface   for Custom User Feature Added

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin 
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('id',"fullname","email", "is_staff", "is_active",'is_superuser',"created_at","updated_at")
+    list_filter = ("email", "is_staff", "is_active",)
+    fieldsets = (
+        (None, {"fields": ("fullname","email")}),
+        ("Permissions", {"fields": ("is_staff", "is_active",'is_superuser', "groups", "user_permissions")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "fullname","email","password1","password2", "is_staff",
+                "is_active",'is_superuser', "groups", "user_permissions"
+            )}
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
