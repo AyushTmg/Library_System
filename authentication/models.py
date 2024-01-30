3bbc8d4d9cb1ebb7f5b1ab2c40932b71389a3f58
AyushTmg <ayushtmg20040722@gmail.com>
2024-01-30 15:09:03 +0545

    Fix: Small issue of Custom User  field is_active Fixed

--- authentication/models.py
+++ authentication/models.py
@@ -10,7 +10,7 @@ class User(AbstractBaseUser,PermissionsMixin):
     fullname=models.CharField(max_length=150)
     email=models.EmailField(unique=True)
 
-    is_active=models.BooleanField(default=False)
+    is_active=models.BooleanField(default=True)
     is_staff=models.BooleanField(default=False)
     is_superuser=models.BooleanField(default=False)
     created_at=models.DateTimeField(auto_now_add=True)
