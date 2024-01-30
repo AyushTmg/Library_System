b83f5b67191a6ec0e9c1d926ac59ebb621e4313e
AyushTmg <ayushtmg20040722@gmail.com>
2024-01-30 22:54:14 +0545

    Feat: Added basic admin interface for Book, BookDetai,Borrow

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from .models import (
+    Book,
+    BookDetail,
+    BorrowedBook
+)
 
-# Register your models here.
+
+    
+
+
+"""  
+Just Some Basic Admin Panel Customization 
+For Library
+"""
+# !Admin Interface for Book  Model
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['id','title','ISBN','user','genre','published_at']
+    list_per_page=10
+    search_fields=['title']
+
+
+
+
+"""  
+Just Some Basic Admin Panel Customization 
+For Library
+"""
+# !Admin Interface for BookDetail  Model
+@admin.register(BookDetail)
+class BookDetailAdmin(admin.ModelAdmin):
+    list_display = ['pk','book','publisher','language','number_of_pages']
+    list_per_page=10
+    autocomplete_fields=['book']
+
+
+
+
+"""  
+Just Some Basic Admin Panel Customization 
+For Library
+"""
+# !Admin Interface for BookDetail  Model
+@admin.register(BorrowedBook)
+class BoorrowedBooklAdmin(admin.ModelAdmin):
+    list_display = ['user','book','borrowed_at','returned_at']
+    list_per_page=10
+    autocomplete_fields=['book','user']
